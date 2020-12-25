919bc70267be74c691d86eda745b5ef2a1533349
cxxyao2 <cxxyao2@gmail.com>
2020-12-25 16:23:38 -0500

    add customised middleware

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import configureStore from "./store/configureStore";
+import configureStore, from "./store/configureStore";
 import * as actions from "./store/bugs";
 import { projectAdded } from "./store/projects";
 import { userAdded } from "./store/users";
@@ -11,16 +11,16 @@ store.subscribe(() => {
 });
 
 store.dispatch(userAdded({ name: "User 1" }));
-store.dispatch(userAdded({ name: "User 2" }));
-store.dispatch(projectAdded({ name: "Project 1" }));
-store.dispatch(actions.bugAdded({ description: "Bug 1" }));
-store.dispatch(actions.bugAdded({ description: "Bug 2" }));
-store.dispatch(actions.bugAdded({ description: "Bug 3" }));
-store.dispatch(actions.bugAssignedToUser({ bugId: 1, userId: 1 }));
-store.dispatch(actions.bugResolved({ id: 1 }));
+// store.dispatch(userAdded({ name: "User 2" }));
+// store.dispatch(projectAdded({ name: "Project 1" }));
+// store.dispatch(actions.bugAdded({ description: "Bug 1" }));
+// store.dispatch(actions.bugAdded({ description: "Bug 2" }));
+// store.dispatch(actions.bugAdded({ description: "Bug 3" }));
+// store.dispatch(actions.bugAssignedToUser({ bugId: 1, userId: 1 }));
+// store.dispatch(actions.bugResolved({ id: 1 }));
 
-const x = actions.getUnresolvedBugs(store.getState());
-const y = actions.getUnresolvedBugs(store.getState());
+// const x = actions.getUnresolvedBugs(store.getState());
+// const y = actions.getUnresolvedBugs(store.getState());
 
 const bugs = actions.getBugsByUser(1)(store.getState());
 console.log(bugs);
