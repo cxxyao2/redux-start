950b0b96dea20292495a6c7afee94ce8a354387e
cxxyao2 <cxxyao2@gmail.com>
2020-12-24 23:50:26 -0500

    before import createSlice

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,47 +1,36 @@
-// Action Types
+import { createAction, createReducer, createSlice } from "@reduxjs/toolkit";
 
-const BUG_ADDED = "bugAdded";
-const BUG_REMOVED = "bugRemoved";
-const BUG_RESOLVED = "bugResolved";
+createSlice({
+  name: 'bugs',
+  initialState: [],
+  reducers: {
+    // actions => action handlers
+    bugAdded: (bugs, action) => {
+      
+    }
+  }
+});
 
 // Action creators
-export const bugAdded = (description) => {
-  return {
-    type: BUG_ADDED,
-    payload: {
-      description: description,
-    },
-  };
-};
-
-export const bugResolved = (id) => ({
-  type: BUG_RESOLVED,
-  payload: {
-    id,
-  },
-});
+export const bugAdded = createAction("bugAdded"); // type: bugAdded, payload: {...}
+export const bugRemoved = createAction("bugRemoved"); // type: bugRemoved, payload: {...}
+export const bugResolved = createAction("bugResolved"); // type: bugResolved, payload: {...}
 
 // Reducer
 let lastId = 0;
 
-export default function reducer(state = [], action) {
-  switch (action.type) {
-    case BUG_ADDED:
-      return [
-        ...state,
-        {
-          id: ++lastId,
-          description: action.payload.description,
-          resolved: false,
-        },
-      ];
-    case BUG_REMOVED:
-      return state.filter((bug) => bug.id !== action.payload.id);
-    case BUG_RESOLVED:
-      return state.map((bug) =>
-        bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
-      );
-    default:
-      return state;
-  }
-}
+export default createReducer([], {
+  // key: value
+  // actions: functions (event => event handler)
+  [bugAdded.type]: (bugs, action) => {
+    bugs.push({
+      id: ++lastId,
+      description: action.payload.description,
+      resolved: false,
+    });
+  },
+  [bugResolved.type]: (bugs, action) => {
+    const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+    bugs[index].resolved = true;
+  },
+});
